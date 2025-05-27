5b388e5dc7d8473b16c90a565a2aba2786670bfe
Eric Bitria <eric.bitria@gmail.com>
2025-05-27 19:34:35 +0200

    changed names

--- app/src/main/java/eric/bitria/hexon/MainActivity.kt
+++ app/src/main/java/eric/bitria/hexon/MainActivity.kt
@@ -33,6 +33,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        viewModel
+
         setContent {
             HexonTheme {
                 val navController = rememberNavController()
